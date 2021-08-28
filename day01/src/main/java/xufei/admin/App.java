d5a72deff683cc70d5f43bd1b91ed3216073b287
pangqingyun <pangqingyun@126.com>
2021-08-28 16:35:23 +0800

    second commit

--- day01/src/main/java/xufei/admin/App.java
+++ day01/src/main/java/xufei/admin/App.java
@@ -7,5 +7,6 @@ package xufei.admin;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println("你好，Git!!!");
     }
 }
